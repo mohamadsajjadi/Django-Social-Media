cfa86f46e1fff4c09fcf1d0ed746be9c263bebbc
Mohamad <alisajadian49@yahoo.com>
2022-07-28 22:09:56 +0430

    ovverride clen form method

--- A/account/forms.py
+++ A/account/forms.py
@@ -1,7 +1,34 @@
 from django import forms
+from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
 
 
 class UserRegistrationForm(forms.Form):
     username = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}))
     email = forms.EmailField(widget=forms.EmailInput(attrs={'class': 'form-control'}))
-    password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control'}))
+    password1 = forms.CharField(label='Password', widget=forms.PasswordInput(attrs={'class': 'form-control'}))
+    password2 = forms.CharField(label='Confirm Password', widget=forms.PasswordInput(attrs={'class': 'form-control'}))
+
+    def clean_email(self):
+        email = self.cleaned_data['email']
+        user = User.objects.filter(email=email).exists()
+        if user:
+            raise ValidationError('This email is already exist!')
+        return email
+
+    def clean_username(self):
+        username = self.cleaned_data['username']
+        user = User.objects.filter(username=username).exists()
+        if user:
+            raise ValidationError('This username is already exist!')
+        return username
+
+    def clean(self):
+        cd = super().clean()
+        p1 = cd.get('password1')
+        p2 = cd.get('password2')
+        if p1 and p2:
+            if p1 != p2:
+                raise ValidationError("Password must match")
+        else:
+            raise ValidationError("Enter the both of password")
--- A/account/views.py
+++ A/account/views.py
@@ -17,7 +17,7 @@ class RegisterView(View):
         form = self.form_classes(request.POST)
         if form.is_valid():
             cd = form.cleaned_data
-            User.objects.create_user(username=cd['username'], email=cd['email'], password=cd['password'])
+            User.objects.create_user(username=cd['username'], email=cd['email'], password=cd['password1'])
             messages.success(request, 'you registered succefully', 'success')
             return redirect('home:home')
         return render(request, self.templates_name, {"form": form})

f22ce72f70f464e0094ccf4da530dc8225daa8b3
Mohamad <alisajadian49@yahoo.com>
2022-08-02 00:14:22 +0430

    develop 404.html

--- A/post/views.py
+++ A/post/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.views import View
 from .models import Post
 from .forms import CreateUpdatePostForm
@@ -9,13 +9,13 @@ from django.utils.text import slugify
 
 class PostDetails(View):
     def get(self, request, post_id, slug_id):
-        post = Post.objects.get(pk=post_id, slug=slug_id)
+        post = get_object_or_404(Post, pk=post_id, slug=slug_id)
         return render(request, 'post/details.html', {'post': post})
 
 
 class PostDeleteView(LoginRequiredMixin, View):
     def get(self, request, post_id):
-        post = Post.objects.get(id=post_id)
+        post = get_object_or_404(Post, id=post_id)
         if post.author.id == request.user.id:
             post.delete()
             messages.success(request, "Post Deleted Succefully!", 'success')
@@ -28,7 +28,7 @@ class PostUpdateView(LoginRequiredMixin, View):
     form_classes = CreateUpdatePostForm
 
     def setup(self, request, *args, **kwargs):
-        self.post_instance = Post.objects.get(pk=kwargs['post_id'])
+        self.post_instance =get_object_or_404(Post,pk=kwargs['post_id'])
         return super().setup(request, *args, **kwargs)
 
     def dispatch(self, request, *args, **kwargs):

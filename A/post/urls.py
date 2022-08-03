fb700924652145715a65370885d816e1f5aaa51f
Mohamad <alisajadian49@yahoo.com>
2022-08-03 21:59:34 +0430

    complete reply comment

--- A/post/urls.py
+++ A/post/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('<int:post_id>/<slug:slug_id>/', views.PostDetails.as_view(), name='post_detail'),
     path('delete/<int:post_id>/', views.PostDeleteView.as_view(), name='post_delete'),
     path('update/<int:post_id>/', views.PostUpdateView.as_view(), name='post_update'),
-    path('create/', views.CreatePostView.as_view(), name='post_create')
+    path('create/', views.CreatePostView.as_view(), name='post_create'),
+    path('reply/<int:post_id>/<int:comment_id>/', views.CommentReplyView.as_view(), name='add_reply')
 ]
--- A/post/views.py
+++ A/post/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.views import View
-from .models import Post
-from .forms import CreateUpdatePostForm, CommentCreateForm
+from .models import Post, Comment
+from .forms import CreateUpdatePostForm, CommentCreateForm, CommentReplyForm
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib import messages
 from django.utils.text import slugify
@@ -11,6 +11,7 @@ from django.utils.decorators import method_decorator
 
 class PostDetails(View):
     form_class = CommentCreateForm
+    form_class_reply = CommentReplyForm
 
     def dispatch(self, request, *args, **kwargs):
         self.post_instance = get_object_or_404(Post, pk=kwargs['post_id'], slug=kwargs['slug_id'])
@@ -19,7 +20,8 @@ class PostDetails(View):
     def get(self, request, *args, **kwargs):
         form = self.form_class
         comments = self.post_instance.pcomment.filter(is_reply=False)
-        return render(request, 'post/details.html', {'post': self.post_instance, 'comments': comments, 'form': form})
+        return render(request, 'post/details.html', {'post': self.post_instance, 'comments': comments, 'form': form,
+                                                     'reply_form': self.form_class_reply})
 
     @method_decorator(login_required)
     def post(self, request, *args, **kwargs):
@@ -90,3 +92,21 @@ class CreatePostView(LoginRequiredMixin, View):
             new_post.save()
             messages.success(request, 'Post successfully added to your profile', 'success')
             return redirect('post:post_detail', new_post.id, new_post.slug)
+
+
+class CommentReplyView(LoginRequiredMixin, View):
+    form_class = CommentReplyForm
+
+    def post(self, request, post_id, comment_id):
+        form = self.form_class(request.POST)
+        post = get_object_or_404(Post, id=post_id)
+        comment = get_object_or_404(Comment, id=comment_id)
+        if form.is_valid():
+            reply = form.save(commit=False)
+            reply.user = request.user
+            reply.post = post
+            reply.reply = comment
+            reply.is_reply = True
+            reply.save()
+            messages.success(request, 'your reply submitted successfully!', 'success')
+        return redirect('post:post_detail', post.id, post.slug)
